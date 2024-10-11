b104593dce21df5069c7819f7f3f9cb3a1482004
SulizLina <alzeghabil@gmail.com>
2024-10-11 15:42:52 +0300

    SDK

--- SAIRProject/SAIRMobileProject/sairdriver/lib/main.dart
+++ SAIRProject/SAIRMobileProject/sairdriver/lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:sairdriver/Splash_Screen.dart';
-import 'package:sairdriver/models/user.dart';
+import 'package:sairdriver/models/user.dart'; // Your custom User class
 import 'package:sairdriver/screens/ViolationsList.dart';
 import 'package:sairdriver/screens/login.dart';
 import 'package:sairdriver/services/auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth; // Alias firebase's User
+//import 'package:flutter_nav/bottom_nav/my_bottom_nav.dart';
+
 void main() async {
   // Ensure that Firebase is initialized before running the app
   WidgetsFlutterBinding.ensureInitialized();
