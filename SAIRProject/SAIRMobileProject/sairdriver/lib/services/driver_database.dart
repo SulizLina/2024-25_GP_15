b15b39c479228ce5542c73ae40100ba804f72e59
SulizLina <alzeghabil@gmail.com>
2024-10-13 21:21:03 +0300

    Debug

--- SAIRProject/SAIRMobileProject/sairdriver/lib/services/driver_database.dart
+++ SAIRProject/SAIRMobileProject/sairdriver/lib/services/driver_database.dart
@@ -33,6 +33,8 @@ class DriverDatabase {
       return null; // Return null on error
     }
   }
+
+  
   /*final CollectionReference motorcycleCollection =
       FirebaseFirestore.instance.collection('Motorcycle');
 
--- SAIRProject/SAIRMobileProject/sairdriver/lib/services/motorcycle_database.dart
+++ SAIRProject/SAIRMobileProject/sairdriver/lib/services/motorcycle_database.dart
@@ -1,26 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:sairdriver/models/driver.dart';
-import 'package:sairdriver/models/driver.dart';
+import 'driver_database.dart';
+
+
 class MotorcycleDatabase {
-  
-  final CollectionReference motorcycleCollection =
-      FirebaseFirestore.instance.collection('Motorcycle');
+  final CollectionReference motorcycleCollection = FirebaseFirestore.instance.collection('Motorcycle');
 
   // Fetch the plate number from the Motorcycle collection using DriverID
   Future<String?> getPlateNumberByDriverId(String driverId) async {
     try {
+      // Debugging output
+      print('Fetching motorcycle for DriverID: $driverId');
+
       QuerySnapshot querySnapshot = await motorcycleCollection
           .where('DriverID', isEqualTo: driverId)
           .limit(1)
           .get();
+
       if (querySnapshot.docs.isNotEmpty) {
         var plateNumber = querySnapshot.docs.first['LicensePlate'];
+        print('Plate number found: $plateNumber'); // Debugging output
         return plateNumber;
+      } else {
+        print('No motorcycle found for this driver.'); // Debugging output
+        return null;
       }
     } catch (e) {
-      print('Error fetching plate number: $e');
+      print('Error fetching plate number: $e'); // Debugging output
       return null;
     }
-    return null;
   }
 }
