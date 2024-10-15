be895896784e34e621fc288832a762ba7fb331e4
RawanSaud23 <srawanalsaif@gmail.com>
2024-10-15 19:37:19 +0300

    home UI

--- SAIRProject/SAIRMobileProject/sairdriver/lib/screens/home.dart
+++ SAIRProject/SAIRMobileProject/sairdriver/lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sairdriver/services/driver_database.dart';
 
 class Home extends StatefulWidget {
   final String driverId;  // DriverID passed from previous page
@@ -9,8 +11,67 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
-    return Text('home');
+    return Scaffold(
+      backgroundColor:  Color.fromARGB(255, 3, 152, 85), 
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Color.fromARGB(255, 3, 152, 85), 
+        shape: const RoundedRectangleBorder(),
+        toolbarHeight: 120, // Adjust the toolbar height
+        iconTheme: const IconThemeData(color: Color(0xFF211D1D)),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(left: 7),
+                ),
+                Transform.translate(
+                  offset: const Offset(0, 10), // Move the text down by 10 pixels to match the home page
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 5),
+                    child: Text(
+                      "Hello USER", ////////////////////////FROM DB!!//////////////
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.only(top: 16.0),
+        decoration: const BoxDecoration(
+          color: Colors.white, // White background for the content
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30), // Rounded top-left corner
+            topRight: Radius.circular(30), // Rounded top-right corner
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 16.0), // Add left padding here
+          child: Center(
+            child: Text(
+              'This is a View crashes page. Will be done in Sprint 2',
+              style: GoogleFonts.poppins(fontSize: 20, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
