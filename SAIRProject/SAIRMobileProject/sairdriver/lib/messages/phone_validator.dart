8106409dbbe4a1330f61c20a1f8216fc6aca7729
SulizLina <alzeghabil@gmail.com>
2024-10-15 20:41:42 +0300

    Validator

--- SAIRProject/SAIRMobileProject/sairdriver/lib/messages/phone_validator.dart
+++ SAIRProject/SAIRMobileProject/sairdriver/lib/messages/phone_validator.dart
@@ -3,8 +3,8 @@ String? validatePhoneNumber(String? value) {
     return 'Please enter your phone number';
   } 
   // Regular expression to match phone numbers starting with country code (e.g., +966) and containing digits only
-  else if (!RegExp(r'^\+9665\d{8}$').hasMatch(value)) { 
-    return 'Enter a valid phone number with country code \n (e.g., +966XXXXXXXXX)';
+  else if (!RegExp(r'^\+966\d{9}$').hasMatch(value)) { 
+    return 'Invalid phone number it must start with +966 and be followed by 9 digits';
   }
   return null;
 }
