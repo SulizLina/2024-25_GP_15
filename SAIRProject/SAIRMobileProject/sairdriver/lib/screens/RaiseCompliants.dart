14ded8ba201300eb730752df81911c84f3cf9daf
RawanSaud23 <srawanalsaif@gmail.com>
2024-11-03 23:30:39 +0300

    debugging

--- SAIRProject/SAIRMobileProject/sairdriver/lib/screens/RaiseCompliants.dart
+++ SAIRProject/SAIRMobileProject/sairdriver/lib/screens/RaiseCompliants.dart
@@ -4,6 +4,7 @@ import 'package:sairdriver/models/violation.dart';
 import 'package:sairdriver/models/complaint.dart';
 import 'package:sairdriver/services/Complaint_database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 
 class Raisecomplaint extends StatefulWidget {
   final Violation violation;
@@ -97,112 +98,123 @@ class _RaisecomplaintState extends State<Raisecomplaint> {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.only(top: 16.0),
-          decoration: const BoxDecoration(
-            color: Color(0xFFFAFAFF),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
+      body: GestureDetector(
+        onTap: (){
+          FocusScope.of(context).unfocus();
+        },
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            padding: const EdgeInsets.only(top: 16.0),
+            decoration: const BoxDecoration(
+              color: Color(0xFFFAFAFF),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Raise a Complaint on violation number: ${widget.violation.Vid}',
-                    style: GoogleFonts.poppins(
-                      fontSize: 21,
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromARGB(201, 3, 152, 85),
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'Write below your complaint on the violation,\nyour text should be with 249 character!',
-                    style:
-                        GoogleFonts.poppins(fontSize: 14, color: Colors.grey),
-                  ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    controller: _controller,
-                    maxLines: 5,
-                    decoration: InputDecoration(
-                      labelText: 'Enter Your Complaint',
-                      alignLabelWithHint: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color.fromARGB(201, 3, 152, 85),
-                          width: 1.5,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color.fromARGB(201, 3, 152, 85),
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.red,
-                          width: 1.5,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.red,
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Raise a Complaint on violation number: ${widget.violation.Vid}',
+                      style: GoogleFonts.poppins(
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(201, 3, 152, 85),
                       ),
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your complaint';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    '${_controller.text.length}/$maxChararcter characters',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 12,
+                    SizedBox(height: 8),
+                    Text(
+                      'Describe your complaint about the violation below',
+                      style:
+                          GoogleFonts.poppins(fontSize: 14, color: Colors.grey),
                     ),
-                  ),
-                  SizedBox(height: 32),
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: submitComplaint,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(201, 3, 152, 85),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _controller,
+                      maxLines: 5, // Allow text to wrap within the field
+                      keyboardType: TextInputType
+                          .multiline, // Supports multi-line wrap without newlines
+                      inputFormatters: [
+                        FilteringTextInputFormatter.deny(
+                            RegExp(r'\n')), // Block newline input
+                      ],
+                      decoration: InputDecoration(
+                        labelText: 'Enter Your Complaint',
+                        alignLabelWithHint: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(201, 3, 152, 85),
+                            width: 1.5,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        padding: EdgeInsets.symmetric(vertical: 16),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color.fromARGB(201, 3, 152, 85),
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.red,
+                            width: 1.5,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.red,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
-                      child: Text(
-                        'Submit',
-                        style: GoogleFonts.poppins(
-                          fontSize: 18,
-                          color: Colors.white,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your complaint';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 8),
+                    Text(
+                      '${_controller.text.length}/$maxChararcter characters',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
+                    ),
+                    SizedBox(height: 32),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: submitComplaint,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(201, 3, 152, 85),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                        ),
+                        child: Text(
+                          'Submit',
+                          style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 165),
-                ],
+                    SizedBox(height: 165),
+                  ],
+                ),
               ),
             ),
           ),
