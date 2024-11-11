f9d4465eb290c5f437b0e53f6c9c490796edaa10
SulizLina <144667908+SulizLina@users.noreply.github.com>
2024-11-11 15:08:35 +0300

    Notification Not Done

--- SAIRProject/SAIRMobileProject/sairdriver/lib/main.dart
+++ SAIRProject/SAIRMobileProject/sairdriver/lib/main.dart
@@ -1,13 +1,23 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:sairdriver/screens/welcomepage.dart';
 import 'package:sairdriver/screens/login_email.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+    Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async{
+      print('handlinf a background message ${message.messageId}');
+    }
 void main() async {
   // Ensure that Firebase is initialized before running the app
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseMessaging.instance.getInitialMessage();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MainApp());
 }
 
@@ -46,7 +56,7 @@ class SplashPage extends StatelessWidget {
                 'assets/image/SGreenCircularPattern.png', // Circular pattern background
                 fit: BoxFit.cover, // Ensure the image fills the entire screen
                 width: double.infinity, // Make the image as wide as the screen
-                height: double.infinity, 
+                height: double.infinity,
               ),
             ),
           ),
@@ -54,13 +64,15 @@ class SplashPage extends StatelessWidget {
           Center(
             child: AnimatedSplashScreen(
               duration: 2000,
-              splashIconSize: 160, 
+              splashIconSize: 160,
               splash: Image.asset(
                 'assets/image/SAIRLogo.png', // Logo image
               ),
               nextScreen: LoginEmail(), // The next screen to transition to
-              splashTransition: SplashTransition.scaleTransition, // Add animation for the logo
-              backgroundColor: Colors.transparent, // Make the background transparent to retain the design
+              splashTransition: SplashTransition
+                  .scaleTransition, // Add animation for the logo
+              backgroundColor: Colors
+                  .transparent, // Make the background transparent to retain the design
             ),
           ),
         ],
