a4f4cb40b6f0ee727bc03a054e7d3a72d64c785a
RawanSaud23 <srawanalsaif@gmail.com>
2024-11-13 21:49:03 +0300

    crash notification NOTT done

--- SAIRProject/SAIRMobileProject/sairdriver/lib/models/crash.dart
+++ SAIRProject/SAIRMobileProject/sairdriver/lib/models/crash.dart
@@ -1,62 +1,62 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
 class Crash {
+  String? cDocid;
   String? cid;
   String? driverId;
   String? gspNumber;
-  String? location; 
-  GeoPoint? position; 
-  int? time;  
+  String? location;
+  GeoPoint? position;
+  int? time;
   String? status;
   final Timestamp? timestamp; // Use Timestamp if it comes from Firestore
 
   Crash({
+    required this.cDocid,
     required this.cid,
     required this.driverId,
     required this.gspNumber,
     required this.location,
     required this.position,
     required this.time,
-        required this.status,
-            this.timestamp,
-
-
+    required this.status,
+    this.timestamp,
   });
 
-    String getFormattedDate() {
-      if (time == null) return 'N/A'; // Return N/A if time is not set
-      DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(time! * 1000);
-      return DateFormat('yyyy-MM-dd').format(dateTime); // Only date
-    }
+  String getFormattedDate() {
+    if (time == null) return 'N/A'; // Return N/A if time is not set
+    DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(time! * 1000);
+    return DateFormat('yyyy-MM-dd').format(dateTime); // Only date
+  }
 
-    // Function to get formatted time
-    String getFormattedTimeOnly() {
-      if (time == null) return 'N/A';
-      DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(time! * 1000);
-      return DateFormat('HH:mm:ss').format(dateTime); 
-    }
+  // Function to get formatted time
+  String getFormattedTimeOnly() {
+    if (time == null) return 'N/A';
+    DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(time! * 1000);
+    return DateFormat('HH:mm:ss').format(dateTime);
+  }
 
   factory Crash.fromJson(DocumentSnapshot document) {
     Map<String, dynamic> parsedJSON = document.data() as Map<String, dynamic>;
 
     return Crash(
+      cDocid: document.id, //crash id
       cid: parsedJSON['crashID'].toString(),
       driverId: parsedJSON['driverID'].toString(),
       gspNumber: parsedJSON['GPSnumber'].toString(),
-      status: parsedJSON['Status'] as String?, 
-      location: parsedJSON['location'] as String?, 
+      status: parsedJSON['Status'] as String?,
+      location: parsedJSON['location'] as String?,
       position: parsedJSON['position'] != null
           ? GeoPoint(
-              parsedJSON['position']['latitude'], 
-              parsedJSON['position']['longitude'], 
+              parsedJSON['position']['latitude'],
+              parsedJSON['position']['longitude'],
             )
           : null,
-    
-      time: parsedJSON['time'] as int?,   
-            timestamp: parsedJSON['timestamp'] as Timestamp?, // Ensure the field name matches Firestore
 
+      time: parsedJSON['time'] as int?,
+      timestamp: parsedJSON['timestamp']
+          as Timestamp?, // Ensure the field name matches Firestore
     );
   }
 }
