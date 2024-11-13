4d40464012c22ddae0ebf21572bd634d02b23553
SulizLina <144667908+SulizLina@users.noreply.github.com>
2024-11-13 03:05:33 +0300

    OLD Notification

--- SAIRProject/SAIRMobileProject/sairdriver/lib/main.dart
+++ SAIRProject/SAIRMobileProject/sairdriver/lib/main.dart
@@ -5,33 +5,25 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:sairdriver/screens/welcomepage.dart';
 import 'package:sairdriver/screens/login_email.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:sairdriver/services/NotificationService.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print('handling a background message ${message.messageId}');
+}
 
 void main() async {
   // Ensure that Firebase is initialized before running the app
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  
-  // Register the background message handler
-  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
-  
-  runApp(MainApp());
-}
-
-// Background message handler (called when app is in the background or terminated)
-Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // Handle background messages here (optional logging, etc.)
-  print('Handling a background message: ${message.messageId}');
-  // You can perform additional actions here, like logging or background tasks.
+  await FirebaseMessaging.instance.getInitialMessage();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  final NotificationService _notificationService = NotificationService();
-  
-  MainApp({super.key});
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -39,11 +31,6 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: SplashPage(),
     );
-  }
-
-  // Initialize notifications after Firebase is ready
-  void initNotifications(String driverId) {
-    _notificationService.init(driverId);
   }
 }
 
@@ -83,8 +70,10 @@ class SplashPage extends StatelessWidget {
                 'assets/image/SAIRLogo.png', // Logo image
               ),
               nextScreen: LoginEmail(), // The next screen to transition to
-              splashTransition: SplashTransition.scaleTransition, // Add animation for the logo
-              backgroundColor: Colors.transparent, // Make the background transparent to retain the design
+              splashTransition: SplashTransition
+                  .scaleTransition, // Add animation for the logo
+              backgroundColor: Colors
+                  .transparent, // Make the background transparent to retain the design
             ),
           ),
         ],
